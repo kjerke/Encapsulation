0b65535d8be4d4e7420ec2923c57ac8e012e863b
kevinjerke <kevinjerke@10.30.20.218>
2016-09-01 16:50:21 -0500

    HrDepartment Classes added

---
+++ src/lab1/my/solution/HrDepartment.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1.my.solution;
+
+/**
+ *
+ * @author kevinjerke
+ */
+public class HrDepartment {
+    private String Requirements;
+    
+    
+    
+   private void MeetWithHRAboutSalaryAndBenefits(){
+       System.out.println("Employee Meeting Date With HR has been Set.");
+      
+   }
+   private void MeetDepartmentStaff(){
+        System.out.println("Employees have met with HR Department Staff");
+        
+    }
+   private void ReviewDepartmentPolicies(){
+       System.out.println("Department Policies have been reviewd.");
+   }
+   
+   private void MoveIntoCubical(){
+       System.out.println("Employee Move in Date has been set");
+   }
+    
+    private void GetEmployeeStatus(){
+       System.out.println("Employee Status is Aquired.");
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+}
